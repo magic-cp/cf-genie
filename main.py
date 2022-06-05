49c2700f702ac1bc94b6496101d11072da81df31
German Robayo <germanrobayo33@gmail.com>
2022-06-05 01:58:23 -0400

    chore: gitignore app.logs

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import csv
 import logger
-logger.setup_applevel_logger(is_debug=True)
+logger.setup_applevel_logger(is_debug=True, file_name='app.logs')
+
+from pprint import PrettyPrinter
+
+pprint = PrettyPrinter().pprint
+
 
 import utils
 
 log = logger.get_logger(__name__)
 
-log.info(utils.preprocess_cf_statement('This is a test case!'))
+with open('./dataset/cf_problems.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        pprint(row['statement'])
+        pprint(utils.preprocess_cf_statement(row['statement']))
