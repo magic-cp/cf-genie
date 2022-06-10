fc388bd9a96d5a4479430fb033f4b867fa498090
German Robayo <germanrobayo33@gmail.com>
2022-06-09 23:53:31 -0400

    feat: Improve generate_vscode_launch_json

--- generate_vscode_launch_json.sh
+++ generate_vscode_launch_json.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
+TASKS='[{"type": "python","request": "launch","name": "Run Current File", "module": "cf_genie.tasks.${fileBasenameNoExtension}", "justMyCode": true}'
+
 while read task; do
     # echo "Generating launch.json for task: $task"
     module_name=$(basename $task .py)
     json='{"type": "python","request": "launch","name": "'$module_name'", "module": "cf_genie.tasks.'$module_name'", "justMyCode": true}'
-    tasks="$tasks""$json"","
-    # echo $json
-    # echo $tasks
-    TASKS=$tasks
+    TASKS="$TASKS"",$json"
 done <<< "$(ls ./cf_genie/tasks/*.py)"
 
 # Add a custom one to run the current file
-TASKS='['$TASKS'{"type": "python","request": "launch","name": "Run Current File", "module": "cf_genie.tasks.${fileBasenameNoExtension}", "justMyCode": true}]'
+TASKS=$TASKS']'
 
 jq -n --indent 4 --arg version "0.2.0" --argjson configurations "$TASKS" '$ARGS.named' > .vscode/launch.json
