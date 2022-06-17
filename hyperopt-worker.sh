815116046f1f176adb01e464f2e98361966d26c7
German Robayo <germanrobayo33@gmail.com>
2022-06-17 15:05:44 -0400

    Tune parameters of hyperopt-worker.sh

--- hyperopt-worker.sh
+++ hyperopt-worker.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Starting hyperopt-mongo-worker..."
-PYTHONPATH=. hyperopt-mongo-worker --mongo="localhost:27017/cf_genie" --workdir=$(pwd)
+while true
+do
+    PYTHONPATH=. hyperopt-mongo-worker --mongo="localhost:27017/cf_genie" --workdir=$(pwd) --max-consecutive-failures=10 --poll-interval=10
+done;
+
