3cdb4a03527f43540b20a88b56807e3721edc9d0
German Robayo <germanrobayo33@gmail.com>
2022-07-04 23:18:42 -0400

    use all traininers

--- cf_genie/models/__init__.py
+++ cf_genie/models/__init__.py
@@ -1,15 +1,16 @@
 from typing import Callable, List, Type
 
-from cf_genie.models.base import BaseSupervisedModel, BaseUnSupervisedModel
+from cf_genie.models.base import (BaseSupervisedModel, BaseUnSupervisedModel,
+                                  TrainingMethod)
 from cf_genie.models.complement_naive_bayes import ComplementNaiveBayes
 from cf_genie.models.lstm import LSTM
 from cf_genie.models.multinomial_naive_bayes import MultinomialNaiveBayes
 from cf_genie.models.random_forest import RandomForest
 
 SUPERVISED_MODELS: List[Type[BaseSupervisedModel]] = [
-    # MultinomialNaiveBayes,
-    # ComplementNaiveBayes,
-    # RandomForest,
-    LSTM
+    MultinomialNaiveBayes,
+    ComplementNaiveBayes,
+    RandomForest,
+    LSTM,
 ]
 UNSUPERVISED_MODELS: List[Type[BaseUnSupervisedModel]] = []
