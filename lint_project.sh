b4d66dac98bb1a8831d66ee5b35a56bf26535793
German Robayo <germanrobayo33@gmail.com>
2022-07-05 13:58:58 -0400

    feat: Add task_runner.py

--- lint_project.sh
+++ lint_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-isort --gitignore cf_genie/
-autopep8 --aggressive --aggressive --exclude .env/ --recursive cf_genie/ --in-place
+isort --gitignore task_runner.py cf_genie/
+autopep8 --aggressive --aggressive --exclude .env/ --recursive task_runner.py cf_genie/ --in-place
---
+++ task_runner.py
@@ -0,0 +1,35 @@
+import argparse
+import importlib
+from types import ModuleType
+
+import cf_genie.logger as logger
+from cf_genie.utils import Timer
+
+
+def get_task_module(task):
+    return f'cf_genie.tasks.{task}'
+
+
+def import_module(task: str) -> ModuleType:
+    try:
+        return importlib.import_module(get_task_module(task))
+    except ModuleNotFoundError as e:
+        e.msg = f'Task {task} is not present in the cf_genie.tasks module'
+        raise e
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run a task inside cf_genie.tasks')
+    parser.add_argument('task', type=import_module, help='Python module inside cf_genie.tasks')
+
+    args = parser.parse_args()
+    module: ModuleType = args.task
+    module.__name__
+
+    log = logger.get_logger('task_runner')
+    with Timer(f'Executing `main` function of module {module.__name__}', log=log):
+        module.main()
+
+
+if __name__ == '__main__':
+    main()
