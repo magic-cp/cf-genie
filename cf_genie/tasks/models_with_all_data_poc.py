55dd29ecb33869969e49ee29dfc6690af3815c59
German Robayo <germanrobayo33@gmail.com>
2022-09-05 15:35:59 -0400

    remove commented code

--- cf_genie/tasks/models_with_all_data_poc.py
+++ cf_genie/tasks/models_with_all_data_poc.py
@@ -33,43 +33,6 @@ def main():
     y = df['most_occurrent_tag_group'].to_numpy()
     for run_strategy, model_class in product(RunStrategy, SUPERVISED_MODELS):
         run_model(model_class, y, run_strategy)
-    #     for run_strategy in RunStrategy:
-    # with Timer('Running task ' + __file__, log=log):
-    #     for embedder_class in EMBEDDERS:
-    #         for model_class in SUPERVISED_MODELS:
-    #             with Timer(f'Loading embedded words for {embedder_class.__name__}', log=log):
-    #                 X = embedder_class.read_embedded_words()
-    #             with Timer(f'Training model {model_class.__name__} with embedder {embedder_class.__name__} on all classes', log=log):
-    #                 model = model_class(
-    #                     embedder_class.read_embedded_words,
-    #                     y,
-    #                     TrainingMethod.GRID_SEARCH_CV,
-    #                     label='with-' +
-    #                     embedder_class.__name__ +
-    #                     '-on-all-classes-grid-search-cv')
-    #                 print_train_results(model, X, y)
-    #             for tag_group in utils.TAG_GROUPS:
-    #                 non_tag_group = f'NON_{tag_group}'
-    #                 y_tag_group = np.vectorize(lambda x: tag_group if x == tag_group else non_tag_group)(y)
-    #                 log.info(np.unique(y_tag_group))
-    #                 with Timer(f'Training model {model_class.__name__} with embedder {embedder_class.__name__} on tag group {tag_group} vs others', log=log):
-    #                     model = model_class(
-    #                         embedder_class.read_embedded_words,
-    #                         y_tag_group,
-    #                         label=f'with-{embedder_class.__name__}-on-{tag_group}-vs-rest-classes')
-    #                     print_train_results(model, X, y_tag_group)
-
-    #                 with Timer(f'Training model {model_class.__name__} with embedder {embedder_class.__name__} on all classes except {tag_group} data', log=log):
-    #                     y_not_tag_group = y != tag_group
-
-    #                     def get_x():
-    #                         X = embedder_class.read_embedded_words()
-    #                         return X[y_not_tag_group]
-    #                     model = model_class(
-    #                         get_x,
-    #                         y[y_not_tag_group],
-    #                         label=f'with-{embedder_class.__name__}-without-{tag_group}-class')
-    #                     print_train_results(model, get_x(), y[y_not_tag_group])
 
 
 if __name__ == '__main__':
