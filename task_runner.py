4c52425112743ee58e4e280f36392d744db55bdc
German Robayo <germanrobayo33@gmail.com>
2022-09-05 11:10:43 -0400

    allow task_runner.py to receive arguments that can be passed to other tasks

--- task_runner.py
+++ task_runner.py
@@ -1,6 +1,7 @@
 import argparse
 import importlib
 from types import ModuleType
+from typing import Tuple
 
 import cf_genie.logger as logger
 from cf_genie.utils import Timer
@@ -22,12 +23,21 @@ def import_module(task: str) -> ModuleType:
         e.msg = f'Task {task} is not present in the cf_genie.tasks module'
         raise e
 
+def parse_prog_arg(prog_arg: str) -> Tuple[str, str]:
+    prog_arg_split = prog_arg.split('=')
+    if len(prog_arg_split) != 2:
+        raise ValueError(f"Found more than one '=' in program argument {prog_arg}")
+
+    return prog_arg_split[0], prog_arg_split[1]
 
 def main():
     parser = argparse.ArgumentParser(description='Run a task inside cf_genie.tasks')
     parser.add_argument('task', type=import_module, help='Python module inside cf_genie.tasks')
+    parser.add_argument('--prog-arg', type=parse_prog_arg, help='Task runner arguments in the form of ARG=VALUE', action='append', default=[])
 
     args = parser.parse_args()
+
+    print(args)
     module: ModuleType = args.task
     module.__name__
 
