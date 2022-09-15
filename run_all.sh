3c067b8bbf5c47982df0266dea875646d374baa4
German Robayo <germanrobayo33@gmail.com>
2022-09-15 10:06:20 -0500

    add some tasks to script

--- run_all.sh
+++ run_all.sh
@@ -2,9 +2,21 @@
 
 DATASET_GENERATION_TASKS="
 load_cf_data
+generate_temp_input_for_raw_dataset
 scrap_dataset
 cleanup_dataset
+split_training_and_test
+balance_dataset
+embed_datasets
 "
+
+PLOTTING_AND_ANALYSIS_TASKS="
+generate_wordclouds
+plot_statistics_for_tag_group
+result_analysis
+run_stats_on_embedded_data
+"
+
 function run_tasks() {
     for task in $1; do
         echo "Running $task"
