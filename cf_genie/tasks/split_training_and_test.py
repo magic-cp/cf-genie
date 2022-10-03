0c21d5d1b93a491a25fa6b57fba0fb882c9a4729
German Robayo <germanrobayo33@gmail.com>
2022-10-03 02:29:17 -0400

    make split_training_and_test receive name_suffix as arg

--- cf_genie/tasks/split_training_and_test.py
+++ cf_genie/tasks/split_training_and_test.py
@@ -19,12 +19,13 @@ def parse_args(args):
     parser = argparse.ArgumentParser(
         description='Splits the dataset into training and test split')
     parser.add_argument('--test-size', type=float, help='Test percentage size as a float', default=0.40)
+    parser.add_argument('--name-suffix', type=str, help='Name suffix of the dataset', default='without-adhoc', const='', nargs='?')
     return parser.parse_args(args)
 
 
 def main(*args):
     args = parse_args(args)
-    name_suffix = 'without-adhoc'
+    name_suffix = args.name_suffix
     df = utils.read_cleaned_dataset(name_suffix=name_suffix)
 
     log.info('Raw dataset shape: %s', df.shape)
@@ -46,8 +47,8 @@ def main(*args):
     log.info('Value counts of test dataset as percentages: %s',
              df_test['most_occurrent_tag_group'].value_counts(normalize=True))
 
-    utils.write_cleaned_dataframe_to_csv(df_test, name_suffix=name_suffix + '-test')
-    utils.write_cleaned_dataframe_to_csv(df_train, name_suffix=name_suffix + '-train')
+    utils.write_cleaned_dataframe_to_csv(df_test, name_suffix=name_suffix + '-test' if name_suffix else 'test')
+    utils.write_cleaned_dataframe_to_csv(df_train, name_suffix=name_suffix + '-train' if name_suffix else 'train')
 
 
 if __name__ == '__main__':
